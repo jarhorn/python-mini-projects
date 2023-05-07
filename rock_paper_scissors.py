aa3a602ff2d1f9f8ae23593fb5211c97617a8601
Jason Horn <jarhorn@gmail.com>
2023-05-07 23:44:24 +0000

    updated rock/paper/scissors logic to use modulo operator

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -20,25 +20,21 @@ while True:
     if user_input not in options:
         continue
 
-    random_number = random.randint(0,2)
     # rock = 0, paper = 1, scissors = 2
-    computer_pick = options[random_number]
-    print(f"Computer picked: {computer_pick}.")
+    user_pick_index = options.index(user_input)
+    computer_pick_index = random.randint(0,2)
+    
+    computer_pick = options[computer_pick_index]
+    print(f"Computer picked: {computer_pick}, user picked: {user_input}")
 
-    if user_input == "rock" and computer_pick == "scissors":
-        print("You won!")
-        user_wins += 1
-    elif user_input == "paper" and computer_pick == "rock":
-        print("You won!")
-        user_wins += 1
-    elif user_input == "scissors" and computer_pick == "paper":
-        print("You won!")
-        user_wins += 1
-    elif user_input == computer_pick:
-        print("It's a tie... No winner here!")
-    else:
-        print("You lost!")
+    if user_pick_index == computer_pick_index:
+        print("It's a draw... no winner!")
+    elif (user_pick_index + 1) % 3 == (computer_pick_index % 3):
+        print("Computer wins!")
         computer_wins += 1
+    else:
+        print("User wins!")
+        user_wins += 1
     
 print()
 print("Okay, we're done playing... here's the results:")
